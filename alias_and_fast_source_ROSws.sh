8ed56a3e245a2be0d199b569fd495739a2081f5d
Ryan Chen <ryanchen.ylc@gmail.com>
2022-06-18 00:44:11 +0800

    update fast_source, fix incorrect dir error

--- alias_and_fast_source_ROSws.sh
+++ alias_and_fast_source_ROSws.sh
@@ -2,19 +2,23 @@ cat >>~/.bashrc<<EOF
 
 ### Fast source workspace
 ### // This script should be put above alias setting.
-name=\$(pwd)
-if [ "\$(echo \$name | grep ws)" != "" ];then
-	while [ "\$(dirname \$name | grep ws)" != "" ]
-	do
-		name=\$(dirname \$name)
-	done
-else
-	echo  "[WARNING] Not in a correct directory, source workspace failed."
-fi
+function sourceWS()
+{
+    name=\$(pwd)
+    if [ "\$(echo \$name | grep ws)" != "" ];then
+        while [ "\$(dirname \$name | grep ws)" != "" ]
+        do
+            name=\$(dirname \$name)
+        done
+        echo "source Workspace: \${name}"
+        source \${name}/devel/setup.bash
+    else
+        echo  "[WARNING] Not in a correct directory, source workspace failed."
+    fi
+}
 
 ### Alias
 alias rc='vim ~/.bashrc'
 alias src='source ~/.bashrc'
-alias echoWS='echo "source Workspace: \${name}"'
-alias sw='echoWS && source \${name}/devel/setup.bash'
+alias sw='sourceWS'
 EOF
